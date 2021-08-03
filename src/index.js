d1567569093bb4735c36c8016ae528ff57a082dc
Meet94286 <ermeet8@gmail.com>
2021-08-04 03:44:27 +0530

    Render GridList and Released movies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+import { GridList, ReleasedMovies } from './screens/home/Home';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './common/header/Header';
 import './index.css';
+import Home from './screens/home/Home';
 
 ReactDOM.render(
   <div>
-  <Header/>
+    <Home/>
+    <GridList/>
+    <div className="flex-container">
+      <div className="left">
+        <ReleasedMovies/>
+      </div>
+      <div className="right">
+        </div>
+    </div>
   </div>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
