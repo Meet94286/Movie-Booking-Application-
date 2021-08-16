7d7549f6427b768273c72b3597e16d240e465d67
Meet94286 <ermeet8@gmail.com>
2021-08-16 14:23:23 +0530

    Added FilterCard component with props

--- src/screens/home/ReleasedMovies.jsx
+++ src/screens/home/ReleasedMovies.jsx
@@ -5,15 +5,19 @@ import ImageListItem from '@material-ui/core/ImageListItem';
 import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 import moviesData from '../../common/moviesData';
 import moment from 'moment';
+import { Link } from "react-router-dom";
+import FilterCard from "./FilterCard";
+import { useState } from "react";
+import './Home.css';
 
 
 
 
 const releasedMovieStyles = makeStyles((theme) => ({
     root: {
-      display: 'flex',
+      // display: 'flex',
       flexWrap: 'wrap',
-      justifyContent: 'space-around',
+      // justifyContent: 'space-around',
       overflow: 'hidden',
       backgroundColor: theme.palette.background.paper,
     },
@@ -28,21 +32,123 @@ const releasedMovieStyles = makeStyles((theme) => ({
   
   
   export default function ReleasedMovies() {
-        const classes = releasedMovieStyles();
+        
+    const classes = releasedMovieStyles();
+
+    const[ data,setData]=useState(moviesData);
+  const[movieName,setMovieName]=useState("");
+  const[genreName,setGenreName]= useState([]);
+  const[artistName,setArtistName]= useState([]);
+
+
+   function filterByMovieName(moviesData){
+    if(!movieName){
+      return moviesData;
+    }
+
+    return moviesData.filter((item)=>(item.title.toLowerCase()===movieName.toLowerCase()));
+  }
+
+
+
+  function  filterByGenreName(moviesData){
+    if(genreName.length === 0){
+      return moviesData;
+    }
+    return moviesData.filter((item)=>{
+     for(let i=0;i<item.genres.length;i++){
+           if(genreName.includes(item.genres[i])){
+              return true;
+           }
+     }
+     return false;}
+    );
+    }
+
+
+
+  function  filterByArtistName(moviesData){
+    if(artistName.length === 0){
+      return moviesData;
+    }
+    return moviesData.filter((item)=>{
+      let movieArtists = item.artists.map(
+        (artist) => artist.first_name + " " + artist.last_name
+      );
+     for(let i=0;i<movieArtists.length;i++){
+           if(artistName.includes(movieArtists[i])){
+              return true;
+           }
+     }
+     return false;}
+    );
+    }
+
+    
+   
+   
+
+
+
+  function filterBy(event){
+
+    let filteredMovieByName = filterByMovieName(moviesData);
+    let filteredMovieByGenre = filterByGenreName(filteredMovieByName);
+    let filteredMovieByArtist = filterByArtistName(filteredMovieByGenre);
+    setData(filteredMovieByArtist);
+     
+  }
+  
+
+
+  
+    
     return (
+      <div>
+      <div style={{display:"flex"}}>
+        <div className="left">
       <div className={classes.root}>
+
         <ImageList rowHeight={350} cols={4} className={releasedMovieStyles.imageList}>
     
-        {moviesData.map((item) => (
-            <ImageListItem key={item.img}>
-              <img src={item.poster_url} alt={item.title} />
+        {data.map(
+          (item) => (
+          
+          <ImageListItem key={item.img}  >
+              <Link to={`/details/${item.id}`}>
+              <img src={item.poster_url} style={{width: "100%"}} alt={item.title} />
+              
+
               <ImageListItemBar
                 title={item.title}
                 subtitle={<span>Release Date: {moment(item.release_date).format("ddd ll")}</span>}/>
+                </Link>
             </ImageListItem>
-          ))}
+            ))
+            }
         </ImageList>
+        
       </div>
+      </div>
+
+      <div className="right">
+       <FilterCard
+       movieName={movieName}
+       setMovieName={setMovieName}
+       filterBy={filterBy}
+       genreName={genreName}
+       setGenreName={setGenreName}
+       artistName={artistName}
+       setArtistName={setArtistName}
+       
+       />
+
+      </div>
+
+
+      </div>
+      </div>
+      
     );
   }
   
