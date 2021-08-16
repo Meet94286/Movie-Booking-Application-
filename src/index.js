b8c102414d561d65bef86075230bfe3bd7e189e0
Meet94286 <ermeet8@gmail.com>
2021-08-16 14:28:46 +0530

    Routing details page

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Details from './screens/details/Details';
 import Home from './screens/home/Home';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import FilterCard from './screens/home/FilterCard';
-import ReleasedMovies from './screens/home/ReleasedMovies';
-import UpcomingMovies from './screens/home/UpcomingMovies';
+
+
 ReactDOM.render(
-  <div>
-    <Home/>
-    <UpcomingMovies/>
-    <div className="flex-container">
-      <div className="left">
-        <ReleasedMovies/>
-      </div>
-      <div className="right">
-        <FilterCard/>
-        </div>
-    </div>
-  </div>, document.getElementById('root'));
+  <BrowserRouter>
+     <div>
+      
+     <Route exact path ="/" component={Home}/>
+     <Route exact path="/details/:id"  component={Details}/>
+     
+     </div>
+   </BrowserRouter>
+   , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
