5bd5ed0899ace4a43a76b9919bc418e88d4c033c
Meet94286 <mehtameet4321.mm@gmail.com>
2021-08-19 01:38:26 +0530

    Modified Route changes

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import Home from './screens/home/Home';
 import {BrowserRouter, Route} from "react-router-dom";
 import BookShow from './screens/bookshow/BookShow';
 
-
+const baseUrl = "http://localhost:8085/api/"
 ReactDOM.render(
   <BrowserRouter>
      <div>
       
      <Route exact path ="/" component={Home}/>
-     <Route exact path="/details/:id"  component={Details}/>
+     <Route exact path="/details/:id"  component={Details}  />
      <Route exact path="/bookshow/:id"  component={BookShow}/>
   
     
